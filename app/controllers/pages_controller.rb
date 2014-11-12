96005f5d6c33d3193dc5649cde088594815a5057
Sameer Siruguri <siruguri@gmail.com>
2014-11-12 11:14:00 -0800

    results page has content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,7 +15,14 @@ class PagesController < ApplicationController
                       next_page: 3,
                      },
 
-                     3 => {},
+                     3 => {
+                       results_header1: 'Health Plan', results_header2: 'By The Numbers', results_header3: 'The Bottom Line',
+                       checkbox_list: [{label: "I'm a smoker"}, {label: "I have an ongoing illness"}, {label: "I take prescription medication"}, {label: "I have a favorite doctor"},],
+                       results_data: [
+                         {image: "aetna_logo.png", first_line: "Monthly Premium $871", result_deets: "Typical Drug Cost (Per refill): $50<br>\nPrimary Doctor Visit: $40<br>\nDoctors Nearby 490", last_line: "True Cost of Annual Care: $15,603", first_line2: "Recommended because:", result_deets2: "This will allow you to cover the medication you need.", plantype: 'HMO'},
+                         {image: "kp_logo_transparent.gif", first_line: "Monthly Premium $556", result_deets: "Typical Drug Cost (Per refill): $60<br>\nPrimary Doctor Visit: $60<br>\nDoctors Nearby 1225", last_line: "True Cost of Annual Care: $18,233", first_line2: "Recommended because:", result_deets2: "Your doctors are in this network.", plantype: 'PPO'}
+                       ]
+                     },
                     }
   
   def show
