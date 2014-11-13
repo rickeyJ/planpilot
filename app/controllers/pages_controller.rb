37024a0843286d45a33dc640efe864eb71030f53
Sameer Siruguri <siruguri@gmail.com>
2014-11-13 10:07:06 -0800

    changed popup order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,7 +17,7 @@ class PagesController < ApplicationController
 
                      3 => {
                        results_header1: 'Health Plan', results_header2: 'By The Numbers', results_header3: 'The Bottom Line',
-                       checkbox_list: [{label: "I'm a smoker", popup_html: '<input class="doctornameinput inline form-control" type="text" placeholder="Enter doctor name"><button class="btn btn-default submit">Go</button>'}, {label: "I have an ongoing illness"}, {label: "I take prescription medication"}, {label: "I have a favorite doctor"},],
+                       checkbox_list: [{label: "I have a favorite doctor", popup_html: '<input class="doctornameinput inline form-control" type="text" placeholder="Enter doctor name"><button class="btn btn-default submit">Go</button>'}, {label: "I have an ongoing illness"}, {label: "I take prescription medication"}, {label: "I'm a smoker"},],
                        results_data: [
                          {image: "aetna_logo.png", first_line: "Monthly Premium $871", result_deets: "Typical Drug Cost (Per refill): $50<br>\nPrimary Doctor Visit: $40<br>\nDoctors Nearby 490", last_line: "True Cost of Annual Care: $15,603", first_line2: "Recommended because:", result_deets2: "This will allow you to cover the medication you need.", plantype: 'HMO'},
                          {image: "kp_logo_transparent.gif", first_line: "Monthly Premium $556", result_deets: "Typical Drug Cost (Per refill): $60<br>\nPrimary Doctor Visit: $60<br>\nDoctors Nearby 1225", last_line: "True Cost of Annual Care: $18,233", first_line2: "Recommended because:", result_deets2: "Your doctors are in this network.", plantype: 'PPO'}
