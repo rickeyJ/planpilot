a465bf4db0a0e3ffe865621ab9107d93d568450b
Sameer Siruguri <siruguri@gmail.com>
2014-11-24 10:21:16 -0800

    bug in plan id attr name

--- scripts/read_hc_data.rb
+++ scripts/read_hc_data.rb
@@ -2,7 +2,7 @@ require_relative './get_hc_columns'
 require 'zlib'
 require 'base64'
 
-ini_keys = ['State', 'County', 'Metal Level', 'Issuer Name', 'Plan_ID', 'Plan_Marketing_Name', 'Plan_Type', 'Rating_Area', 'Child_Only_Offering', 'Source', 'Customer_Service_Phone_Number_Local', 'Customer_Service_Phone_Number_Toll_Free', 'Customer_Service_Phone_Number_TTY', 'Network_URL', 'Plan_Brochure_URL', 'Summary_of_Benefits_URL', 'Drug_Formulary_URL']
+ini_keys = ['State', 'County', 'Metal Level', 'Issuer Name', 'Plan_IDENTIFIER', 'Plan_Marketing_Name', 'Plan_Type', 'Rating_Area', 'Child_Only_Offering', 'Source', 'Customer_Service_Phone_Number_Local', 'Customer_Service_Phone_Number_Toll_Free', 'Customer_Service_Phone_Number_TTY', 'Network_URL', 'Plan_Brochure_URL', 'Summary_of_Benefits_URL', 'Drug_Formulary_URL']
 
 index=0
 
