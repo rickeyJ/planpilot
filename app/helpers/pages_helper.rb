ba6cce37c564afc14ddf16e111c46ea72290cfce
Sameer Siruguri <siruguri@gmail.com>
2014-12-07 13:25:11 -0800

    bug in displaying current info session data

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,17 +2,17 @@ module PagesHelper
 
   def show_current_info(current_info)
     str=''
-    if current_info[:county]
-      str += "#{current_info[:county].gsub(/ [cC]ounty/, '')}, #{current_info[:state]} | "
+    if current_info['county']
+      str += "#{current_info['county'].gsub(/ [cC]ounty/, '')}, #{current_info['state']} | "
     end
-    if current_info[:shop_for]
-      str += "#{current_info[:shop_for]} | "
+    if current_info['shop_for']
+      str += "#{current_info['shop_for']} | "
     end
-    if current_info[:age]
-      str += "#{current_info[:age]} | "
+    if current_info['age']
+      str += "#{current_info['age']} | "
     end
-    if current_info[:marital_status]
-      str += "#{current_info[:marital_status]} | "
+    if current_info['marital_status']
+      str += "#{current_info['marital_status']} | "
     end
 
     str
