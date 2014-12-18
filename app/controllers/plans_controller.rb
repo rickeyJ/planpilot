804cd297c26f2284ea7fb3f1aad8039e0896574b
Sameer Siruguri <siruguri@gmail.com>
2014-12-17 18:48:40 -0800

    interstitial for plan information now has appropriate links

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,8 +1,6 @@
 class PlansController < ApplicationController
   def show
     @plan = Plan.find_by_id params[:id]
-    puts ">>> got back #{session}"
-
     @cms_data = CmsData.make_hash
     @plan_data = @plan.extract_data_for_person(session[:consumer_info], session[:drug_info], session[:pd_info])
   end
