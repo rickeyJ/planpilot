600ce7bca6995798f53b868246594d5094e6a8e5
Sameer Siruguri <siruguri@gmail.com>
2015-06-08 16:39:16 -0700

    Removed files dependent on cancan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-  load_and_authorize_resource
+
   def index
-    @users=User.all
+    if current_user and current_user.admin?
+      @users=User.all
+    else
+      @users=[]
+    end
   end
 
 end
